9bb931e05befe0bfd614d7f08401ea9daa68e19f
Bellanov Apilli <BellanovApilli@gmail.com>
2023-01-27 15:03:50 -0500

    Added Google Provider

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.50.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}
 
 resource "google_storage_bucket" "auto-expire" {
   name          = "no-public-access-bucket"
