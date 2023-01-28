b2de01649c7eaa287d109c3219a671c1f4bd7fa7
Bellanov Apilli <BellanovApilli@gmail.com>
2023-01-28 09:19:09 -0500

    ALX-2: Associated storage infrastructure with a GCP project

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -7,6 +7,7 @@ resource "random_string" "code" {
 
 resource "google_storage_bucket" "log_bucket" {
   name          = "logs-${random_string.code.result}"
+  project       = var.project
   location      = "US"
   force_destroy = true
 
@@ -15,6 +16,7 @@ resource "google_storage_bucket" "log_bucket" {
 
 resource "google_storage_bucket" "release_bucket" {
   name          = "releases-${random_string.code.result}"
+  project       = var.project
   location      = "US"
   force_destroy = true
 
