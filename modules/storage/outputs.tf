292f6a63df3aa587429e290407b85b404a31c602
Bellanov Apilli <BellanovApilli@gmail.com>
2023-01-28 13:54:19 -0500

    ALX-3: terraform fmt

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,10 +1,10 @@
 
 output "log_bucket" {
-    description = "Logs Bucket."
-  value = google_storage_bucket.log_bucket.id
+  description = "Logs Bucket."
+  value       = google_storage_bucket.log_bucket.id
 }
 
 output "release_bucket" {
-    description = "Releases Bucket"
-  value = google_storage_bucket.release_bucket.id
+  description = "Releases Bucket"
+  value       = google_storage_bucket.release_bucket.id
 }
