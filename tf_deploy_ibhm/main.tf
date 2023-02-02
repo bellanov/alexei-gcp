c8d595d2931de03b3bd957463fb47c35292ef6c3
Bellanov Apilli <BellanovApilli@gmail.com>
2023-02-02 00:57:00 -0500

    ALX-8: Cleaned up locals

--- tf_deploy_ibhm/main.tf
+++ tf_deploy_ibhm/main.tf
@@ -24,10 +24,8 @@ module "storage" {
 locals {
   region   = "us-east1"
   project = "development-1675315269"
-  org_id = "105637539410"
   zone     = "us-east1-b"
   location = "US"
-  apis = []
 
   manifest = {
     "dev" : {},
