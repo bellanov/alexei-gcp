8e6a4750db7b1aa48b2f4fd05a7f4a77c1ee81c4
Bellanov Apilli <BellanovApilli@gmail.com>
2023-02-27 21:05:04 -0500

    BELLA-7: Enable services script

--- scripts/create_environment.sh
+++ scripts/create_environment.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 #
 # Create project folder to house a Customer Environment.
-PROJECT_ID='helldivers'
-ORGANIZATION_ID='105637539410'
+PROJECT_ID="helldivers"
+ORGANIZATION_ID="105637539410"
 TIMESTAMP=$(date +%s)
 
 echo "Creating project..."
-echo "PROJECT ID: $PROJECT_ID"
+echo "PROJECT ID: $PROJECT_ID-${TIMESTAMP}"
 
 # Create client environment
 gcloud projects create ${PROJECT_ID}-${TIMESTAMP} \
     --organization=${ORGANIZATION_ID} \
     --name=${PROJECT_ID}
 
-### ENABLE BILLING IN THE PROJECT ###
+# Enable billing in the project (CHASE)
+gcloud beta billing projects link ${PROJECT_ID}-${TIMESTAMP} --billing-account 014920-61CF35-872C41
+
+# Enable project services
+gcloud services enable --project ${PROJECT_ID}-${TIMESTAMP} \
+    iam.googleapis.com \
+    secretmanager.googleapis.com
