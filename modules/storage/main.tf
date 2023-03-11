bfda752820cc4af86f9eb9fb806981aa5bc0a188
Bellanov Apilli <bellanovapilli@gmail.com>
2023-03-11 16:39:47 -0500

    BELLA-20: Refactor Checkpoint

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -5,6 +5,15 @@ resource "random_string" "code" {
   special = false
 }
 
+resource "google_storage_bucket" "logs" {
+  name          = "logs-${random_string.code.result}"
+  project       = var.project
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
+
 resource "google_storage_bucket" "releases" {
   name          = "releases-${random_string.code.result}"
   project       = var.project
