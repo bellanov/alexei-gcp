94651477738c90a243bbd429ff948ba70774cfd4
Bellanov Apilli <bellanovapilli@gmail.com>
2023-03-12 13:08:48 -0400

    BELLA-20: Refactored customer script

--- scripts/create_customer.sh
+++ scripts/create_customer.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
 # Create project folder to house a Customer Environment.
-PROJECT_ID='development'
+CUSTOMER_ID='development'
 ORGANIZATION_ID='105637539410'
 TIMESTAMP=$(date +%s)
 
 echo "Creating project..."
-echo "PROJECT ID: $PROJECT_ID"
+echo "PROJECT ID: $CUSTOMER_ID"
 
 # Create development area
-gcloud projects create ${PROJECT_ID}-${TIMESTAMP} \
+gcloud projects create ${CUSTOMER_ID}-${TIMESTAMP} \
     --organization=${ORGANIZATION_ID} \
-    --name=${PROJECT_ID}
+    --name=${CUSTOMER_ID}
 
 # TODO: Enable any other necessary APIs here
 
