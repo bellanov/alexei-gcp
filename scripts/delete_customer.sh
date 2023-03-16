423cfd9b898824b3d62957d6b670a4069d018db8
Bellanov Apilli <bellanovapilli@gmail.com>
2023-03-16 00:32:52 -0400

    BELLA-20: Added quiet flag to delete script

--- scripts/delete_customer.sh
+++ scripts/delete_customer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 # Delete a customer environment.
-PROJECT_ID='bella'
+PROJECT_ID='ibhm-1678866092'
 
 echo "Deleting project..."
 echo "PROJECT ID: $PROJECT_ID"
 
-gcloud projects delete $PROJECT_ID
+gcloud projects delete $PROJECT_ID --quiet
