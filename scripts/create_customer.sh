85043605288c4254376f1bb7519f9e20d93fa3e6
Bellanov Apilli <bellanovapilli@gmail.com>
2023-04-24 23:21:12 -0400

    BELLA-20: Parameterized iam-policy roles

--- scripts/create_customer.sh
+++ scripts/create_customer.sh
@@ -38,11 +38,13 @@ do
         --iam-account=${SERVICE_ACCOUNT}@${PROJECT_ID}.iam.gserviceaccount.com
 done
 
+TERRAFORM_ROLES="roles/cloudbuild.builds.editor"
 echo "Assigning User Role(s): Cloud Build User"
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --member=serviceAccount:cloud-build@${PROJECT_ID}.iam.gserviceaccount.com \
     --role=roles/cloudbuild.builds.editor
 
+TERRAFORM_ROLES="roles/storage.admin"
 echo "Assigning User Role(s): Terraform User"
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
     --member=serviceAccount:terraform@${PROJECT_ID}.iam.gserviceaccount.com \
