27a44f57011a4a2aa0e0781fbf00330ced507525
Bellanov Apilli <bellanovapilli@gmail.com>
2023-04-24 21:26:01 -0400

    BELLA-20: Fixed incorrect variable name

--- tf_deploy_fantasy/variables.tf
+++ tf_deploy_fantasy/variables.tf
@@ -1,5 +1,10 @@
 
-variable "gcp-creds" {
+variable "gcp_creds" {
   description = "GCP Credentials."
+  default     = ""
+}
+
+variable "github_creds" {
+  description = "GitHub Credentials."
   default     = ""
 }
