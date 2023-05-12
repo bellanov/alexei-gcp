0d06cc9d63c699c5d06e206c6585f0e352a8d78a
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-12 00:49:37 -0400

    BELLA-25: Added for_each to cloud functions

--- modules/application/outputs.tf
+++ modules/application/outputs.tf
@@ -3,8 +3,3 @@ output "name" {
   description = "Cloud Function Name."
   value = [ for function in google_cloudfunctions_function.function: function.name]
 }
-
-output "runtime" {
-  description = "Cloud Function Runtime."
-  value = [ for function in google_cloudfunctions_function.function: function.runtime]
-}
