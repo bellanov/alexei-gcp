968ae4a5d9e6c3370eb7a3a00a0f2f1c2fa9f519
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-14 13:05:02 -0400

    BELLA-26: Enabled Artifact Registry API

--- scripts/enable_apis.sh
+++ scripts/enable_apis.sh
@@ -4,10 +4,13 @@
 # Enable Service APIs in a GCP Project.
 
 PROJECT_ID=$1
-API=$2
+APIS="artifactregistry.googleapis.com cloudbuild.googleapis.com cloudresourcemanager.googleapis.com iam.googleapis.com secretmanager.googleapis.com"
 
 echo "Executing script: $0"
 echo "GCP project: $PROJECT_ID"
-echo "API: $API"
 
-gcloud services enable $API --project $PROJECT_ID
+for API in $APIS
+do
+    echo "Enabling API: $API"
+    gcloud services enable $API --project $PROJECT_ID
+done
