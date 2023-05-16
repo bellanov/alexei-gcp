9a7157828a29fba2ad321c1cb6475601b900d22b
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-15 22:33:31 -0400

    BELLA-27: Added security module to outputs

--- tf_deploy_bellanov/outputs.tf
+++ tf_deploy_bellanov/outputs.tf
@@ -3,3 +3,13 @@ output "storage" {
   description = "Storage Module."
   value       = module.storage
 }
+
+output "application" {
+  description = "Application Module."
+  value       = module.application
+}
+
+output "security" {
+  description = "Security Module."
+  value       = module.security
+}
