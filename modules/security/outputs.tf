d4f333fd58ec444aa94ad0df094959c2d552f324
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-15 22:26:04 -0400

    BELLA-27: Updated service account output value

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -2,5 +2,5 @@
 
 output "service_accounts" {
   description = "Service Accounts."
-  value       = [ for sa in google_service_account.sa : sa.account_id ]
+  value       = [ for sa in google_service_account.sa : sa.email ]
 }
