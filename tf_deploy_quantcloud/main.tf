111a95b48af6ef00752a499f4b74163a7dbe705d
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-16 00:04:25 -0400

    BELLA-27: And then there were six

---
+++ tf_deploy_quantcloud/main.tf
@@ -0,0 +1,64 @@
+// FantasyAce
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.50.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = local.project
+  region      = local.region
+  zone        = local.zone
+  credentials = var.gcp_creds
+}
+
+module "storage" {
+  source   = "../modules/storage"
+  project  = local.project
+  location = local.location
+}
+
+module "security" {
+  source   = "../modules/security"
+  service_accounts = local.security.service_accounts
+}
+
+module "application" {
+  source   = "../modules/application"
+  for_each = local.environments
+
+  release_bucket          = module.storage.releases
+
+  depends_on = [
+    module.storage
+  ]
+}
+
+locals {
+  region   = "us-east1"
+  project  = "fantasyace-1682390017"
+  zone     = "us-east1-b"
+  location = "US"
+
+  security = {
+    "service_accounts": {}
+  }
+
+  environments = {
+    # Development
+    "dev" : {
+      "cloud_run_services" : {}
+    },
+    # Quality Assurance
+    "qa" : {
+      "cloud_run_services" : {}
+    },
+    # Production
+    "prod" : {
+      "cloud_run_services" : {}
+    }
+  }
+}
