3f6c6dd09f2b35daefaa7275ee332c0c4bf81aa5
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-19 11:45:04 -0400

    BELLA-27: Parameterized project references

--- tf_deploy_bellanov/main.tf
+++ tf_deploy_bellanov/main.tf
@@ -64,14 +64,14 @@ locals {
     "dev" : {
       "cloud_run_services" : {
         "editor": {
-          "image": "us-central1-docker.pkg.dev/fantasyace-1682390017/docker-releases/poc-editor",
+          "image": "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-editor",
           "location": local.cloud_run_services.location,
-          "service_account": "editor-identity@bellanov-1682390142.iam.gserviceaccount.com"
+          "service_account": "editor-identity@${local.project}.iam.gserviceaccount.com"
         },
         "renderer": {
-          "image": "us-central1-docker.pkg.dev/fantasyace-1682390017/docker-releases/poc-renderer",
+          "image": "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-renderer",
           "location": local.cloud_run_services.location,
-          "service_account": "renderer-identity@bellanov-1682390142.iam.gserviceaccount.com"
+          "service_account": "renderer-identity@${local.project}.iam.gserviceaccount.com"
         }
       }
     },
