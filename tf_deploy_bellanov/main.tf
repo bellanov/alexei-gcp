cfc8bb745a6eac64f453a00a8eb1783204397b41
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-19 14:22:05 -0400

    BELLA-27: Cascaded terraform identity to other environments

--- tf_deploy_bellanov/main.tf
+++ tf_deploy_bellanov/main.tf
@@ -25,7 +25,6 @@ module "security" {
   source           = "../modules/security"
   service_accounts = local.security.service_accounts
   terraform_identity = local.security.terraform_identity
-
 }
 
 module "application" {
