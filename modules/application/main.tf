cfdc86a02fd63c5245a787ef33290b7e6cf43933
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-20 22:11:35 -0400

    BELLA-27: Testing dynamic templates

--- modules/application/main.tf
+++ modules/application/main.tf
@@ -3,7 +3,11 @@ resource "google_cloud_run_service" "svc" {
   for_each = var.cloud_run_services
   name     = each.key
   location = each.value.location
-  template = jsonencode(each.value.template)
+
+  dynamic "template" {
+    content = each.value.template
+  }
+
   traffic {
     percent         = 100
     latest_revision = true
