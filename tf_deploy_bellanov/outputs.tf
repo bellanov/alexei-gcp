1516f5c9391d9af445d6265b695dd7535f9999e2
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-20 22:22:26 -0400

    BELLA-27: Removed troubleshooting outputs

--- tf_deploy_bellanov/outputs.tf
+++ tf_deploy_bellanov/outputs.tf
@@ -13,14 +13,3 @@ output "security" {
   description = "Security Module."
   value       = module.security
 }
-
-# Troubleshooting
-output "editor_template" {
-  description = "Editor (Cloud Run Service) Template."
-  value       = local.environments.dev.cloud_run_services.editor.template
-}
-
-output "renderer_template" {
-  description = "Renderer (Cloud Run Service) Template."
-  value       = local.environments.dev.cloud_run_services.renderer.template
-}
