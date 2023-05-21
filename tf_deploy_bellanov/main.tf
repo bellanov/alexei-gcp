dd1bcaaf6f6eef096ba3678ad844bf7df0773f7a
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-20 22:26:33 -0400

    BELLA-27: Reigning things in...

--- tf_deploy_bellanov/main.tf
+++ tf_deploy_bellanov/main.tf
@@ -77,13 +77,18 @@ locals {
           "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-editor:0.1.1",
           "location" : local.cloud_run_services.location,
           "service_account" : local.cloud_run_services.editor_identity,
-          "env" : {}
+          "env" : {
+            "EDITOR_UPSTREAM_RENDER_URL" : "/put/these/configurations/in/data/files",
+            "PORT" : "8080"
+          }
         },
         "renderer" : {
           "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-renderer:0.1.1",
           "location" : local.cloud_run_services.location,
           "service_account" : local.cloud_run_services.renderer_identity,
-          "env" : {}
+          "env" : {
+            "PORT" : "8080"
+          }
         }
       }
     },
