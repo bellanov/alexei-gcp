d9e00470fe09c5823b4976b71a72b53c61d52d26
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-21 06:21:22 -0400

    bELLA-27: Cleaning up application module

--- tf_deploy_bellanov/main.tf
+++ tf_deploy_bellanov/main.tf
@@ -29,7 +29,7 @@ module "security" {
 
 resource "google_cloud_run_service" "editor_svc" {
   for_each = local.environments
-  name     = "renderer-svc-${each.key}"
+  name     = "editor-svc-${each.key}"
   location = local.cloud_run_config.location
   template {
     spec {
