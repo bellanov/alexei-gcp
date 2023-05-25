d799746e6752100224c73d35d13a024991e34bc7
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-25 01:35:16 -0400

    ALX-128: Added email to cloudbuild service account

--- tf_deploy_bellanov/main.tf
+++ tf_deploy_bellanov/main.tf
@@ -36,6 +36,7 @@ module "security" {
   project = var.project
   service_accounts   = local.security.service_accounts
   terraform_identity = local.security.terraform_identity
+  # cloudbuild_identity = local.security.terraform_identity
 }
 
 module "build" {
@@ -70,7 +71,8 @@ locals {
     "service_accounts" : {
       "cloudbuild" : {
         "display_name" : "Cloud Build User.",
-        "service_account" : "projects/${local.project}/serviceAccounts/cloudbuild-identity@${local.project}.iam.gserviceaccount.com"
+        "service_account" : "projects/${local.project}/serviceAccounts/cloudbuild-identity@${local.project}.iam.gserviceaccount.com",
+        "email" : "cloudbuild-identity@${local.project}.iam.gserviceaccount.com"
       },
       "editor" : {
         "display_name" : "Service identity of the Editor (Frontend) service.",
