f5e9150e5fece40d07c8e018811752324a54b8a9
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-25 19:32:21 -0400

    ALX-128: Removed unnecessary output

--- modules/role/outputs.tf
+++ modules/role/outputs.tf
@@ -1,6 +1,6 @@
 
 
-output "service_accounts" {
-  description = "Service Accounts."
-  value       = { for sa in google_service_account.sa : sa.account_id => sa.email }
-}
+# output "roles" {
+#   description = "Service Accounts."
+#   value       = { for sa in google_service_account.sa : sa.account_id => sa.email }
+# }
