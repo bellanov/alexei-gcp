7eca00697cf4f2c7a2db32f63a8a708dab919440
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-28 22:42:38 -0400

    ALX-146: Updated roles output format

--- modules/role/outputs.tf
+++ modules/role/outputs.tf
@@ -2,5 +2,5 @@
 
 output "roles" {
   description = "Role Membership."
-  value       = { for member in google_project_iam_member.cloudbuild : member.role => member.etag }
+  value       = [ for member in google_project_iam_member.cloudbuild : member.role ]
 }
