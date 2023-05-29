60212e63a623de27a1a70a8cf1fbe2633738dd66
Bellanov Apilli <bellanovapilli@gmail.com>
2023-05-28 20:07:30 -0400

    ALX-146: Added cloud run jobs to configuration

--- tf_deploy_bellanov/main.tf
+++ tf_deploy_bellanov/main.tf
@@ -158,12 +158,14 @@ locals {
   environments = {
     # Development
     "dev" : {
+      "cloud_run_jobs": {
+        "go-template" : {
+          "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/go-template:0.1.0"
+        }
+      },
       "cloud_run_services" : {
         "editor" : {
           "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-editor:0.3.0"
-        },
-        "go-template" : {
-          "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/go-template:0.1.0"
         },
         "renderer" : {
           "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-renderer:1.2.0"
@@ -172,12 +174,14 @@ locals {
     },
     # Quality Assurance
     "qa" : {
+      "cloud_run_jobs": {
+        "go-template" : {
+          "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/go-template:0.1.0"
+        }
+      },
       "cloud_run_services" : {
         "editor" : {
           "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-editor:0.1.1"
-        },
-        "go-template" : {
-          "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/go-template:0.1.0"
         },
         "renderer" : {
           "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-renderer:0.1.1"
@@ -186,12 +190,14 @@ locals {
     },
     # Production
     "prod" : {
+      "cloud_run_jobs": {
+        "go-template" : {
+          "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/go-template:0.1.0"
+        }
+      },
       "cloud_run_services" : {
         "editor" : {
           "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-editor:0.1.1"
-        },
-        "go-template" : {
-          "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/go-template:0.1.0"
         },
         "renderer" : {
           "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-renderer:0.1.1"
@@ -306,7 +312,7 @@ resource "google_cloud_run_v2_job" "go_template" {
   template {
     template {
       containers {
-        image = each.value.cloud_run_services["go-template"].image
+        image = each.value.cloud_run_jobs["go-template"].image
       }
     }
   }
