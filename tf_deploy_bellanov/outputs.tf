14bdc8b1d44be433d9e0de266d84282e502b82a3
Bellanov Apilli <bellanovapilli@gmail.com>
2023-06-09 08:20:25 -0400

    DEMO: Added editor and renderer outputs

--- tf_deploy_bellanov/outputs.tf
+++ tf_deploy_bellanov/outputs.tf
@@ -18,3 +18,13 @@ output "build" {
   description = "Build Module."
   value       = module.build
 }
+
+output "editor_ui" {
+  description = "Editor UI."
+  value       = { for svc in google_cloud_run_service.editor : svc.id => svc.status }
+}
+
+output "renderer_svc" {
+  description = "Renderer Service."
+  value       = { for svc in google_cloud_run_service.renderer : svc.id => svc.status }
+}
