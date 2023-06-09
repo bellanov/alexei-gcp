24e4b41d231505b8fb2bef949d89d9ae4156870a
Bellanov Apilli <bellanovapilli@gmail.com>
2023-06-09 09:18:54 -0400

    DEMO: Updated editor to 0.7.1

--- tf_deploy_bellanov/main.tf
+++ tf_deploy_bellanov/main.tf
@@ -165,7 +165,7 @@ locals {
       },
       "cloud_run_services" : {
         "editor" : {
-          "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-editor:0.6.3"
+          "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-editor:0.7.1"
         },
         "renderer" : {
           "image" : "us-central1-docker.pkg.dev/${local.project}/docker-releases/poc-renderer:1.3.1"
