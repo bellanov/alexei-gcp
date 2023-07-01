aa2d94b3aefc8e0734faa6bab595510b69690c18
Bellanov Apilli <bellanovapilli@gmail.com>
2023-07-01 19:22:03 -0400

    Added static website bucket resource

--- modules/static_website/main.tf
+++ modules/static_website/main.tf
@@ -4,3 +4,13 @@ resource "random_string" "code" {
   upper   = false
   special = false
 }
+
+resource "google_storage_bucket" "static_website" {
+  name          = "static-website-${random_string.code}"
+  location      = "US"
+  storage_class = "STANDARD"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
--- modules/static_website/outputs.tf
+++ modules/static_website/outputs.tf
@@ -3,3 +3,8 @@ output "name" {
   description = "Website Name."
   value = var.name
 }
+
+output "bucket_name" {
+  description = "Bucket Name."
+  value = google_storage_bucket.static_website.name
+}
