4faf6adf68ae73a826969bdcd43aa3c40a126cea
Bellanov Apilli <bellanovapilli@gmail.com>
2023-07-02 16:11:43 -0400

    Updated incorrect resource name

--- modules/deployment/outputs.tf
+++ modules/deployment/outputs.tf
@@ -1,15 +1,15 @@
 
 output "name" {
   description = "deployment Name."
-  value       = google_clouddeployment_trigger.deployment.name
+  value       = google_cloudbuild_trigger.deployment.name
 }
 
 output "desc" {
   description = "deployment Description."
-  value       = google_clouddeployment_trigger.deployment.description
+  value       = google_cloudbuild_trigger.deployment.description
 }
 
 output "id" {
   description = "deployment Id."
-  value       = google_clouddeployment_trigger.deployment.id
+  value       = google_cloudbuild_trigger.deployment.id
 }
