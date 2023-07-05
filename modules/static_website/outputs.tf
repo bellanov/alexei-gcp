835965c0b540ff1057e2cfc9ead26c0cdca1c069
Bellanov Apilli <bellanovapilli@gmail.com>
2023-07-05 13:11:16 -0400

    Removed unnecessary output

--- modules/static_website/outputs.tf
+++ modules/static_website/outputs.tf
@@ -1,9 +1,4 @@
 
-output "name" {
-  description = "Website Name."
-  value = var.name
-}
-
 output "bucket_name" {
   description = "Bucket Name."
   value = google_storage_bucket.static_website.name
