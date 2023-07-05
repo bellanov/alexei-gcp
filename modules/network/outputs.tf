11f4365a27bc095dd184f8537d4f12e5a4bad3ea
Bellanov Apilli <bellanovapilli@gmail.com>
2023-07-04 20:36:20 -0400

    Fixed incorrect output

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,5 +1,5 @@
 
 output "dns_managed_zones" {
   description = "DNS Managed Zones."
-  value = google_dns_managed_zone.zone.id
+  value       = { for zone in google_dns_managed_zone.zone : zone.name => zone.id }
 }
