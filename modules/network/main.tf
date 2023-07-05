bfd9717a5c43c5752cd74632987d9d110466f37b
Bellanov Apilli <bellanovapilli@gmail.com>
2023-07-04 20:22:29 -0400

    Iterating object

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,6 +1,7 @@
 
 resource "google_dns_managed_zone" "zone" {
-  name        = var.name
-  dns_name    = var.dns_name
+  for_each    = var.dns_managed_zones
+  name        = each.key
+  dns_name    = each.value.dns_name
   description = "Organization DNS Zone."
 }
