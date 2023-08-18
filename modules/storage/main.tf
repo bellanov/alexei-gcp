e2f00beae22f44dc9488a729e6ca9729cd0375db
Bellanov Apilli <bellanovapilli@gmail.com>
2023-08-18 19:52:34 -0400

    Removed unnecessary bucket

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -59,33 +59,6 @@ resource "google_storage_bucket" "releases" {
   }
 }
 
-resource "google_storage_bucket" "connor" {
-  name          = "connor-${random_string.code.result}"
-  project       = var.project
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-
-  lifecycle_rule {
-    condition {
-      age = 30
-    }
-    action {
-      type = "Delete"
-    }
-  }
-
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "AbortIncompleteMultipartUpload"
-    }
-  }
-}
-
 resource "google_artifact_registry_repository" "registry" {
   location      = "us-central1"
   repository_id = "docker-releases"
