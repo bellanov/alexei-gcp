d1e087f2aba8bb8287cc1e8b439bdbf17a26db95
Bellanov Apilli <bellanovapilli@gmail.com>
2023-08-21 22:12:20 -0400

    WIP

--- modules/static_website/main.tf
+++ modules/static_website/main.tf
@@ -36,36 +36,8 @@ resource "google_storage_bucket" "frontend" {
   }
 }
 
-# resource "google_compute_backend_bucket" "backend" {
-#   name        = "backend-bucket-${random_string.code.result}"
-#   bucket_name = google_storage_bucket.image_bucket.name
-#   enable_cdn  = true
-# }
-
-# resource "google_storage_bucket" "backend" {
-#   name          = "backend-${random_string.code.result}"
-#   project       = var.project
-#   location      = "US"
-#   force_destroy = false
-
-#   public_access_prevention = "enforced"
-
-#   lifecycle_rule {
-#     condition {
-#       age = 30
-#     }
-#     action {
-#       type = "Delete"
-#     }
-#   }
-
-#   lifecycle_rule {
-#     condition {
-#       age = 1
-#     }
-#     action {
-#       type = "AbortIncompleteMultipartUpload"
-#     }
-#   }
-# }
-
+resource "google_compute_backend_bucket" "backend" {
+  name        = "backend-${random_string.code.result}"
+  bucket_name = google_storage_bucket.frontend.name
+  enable_cdn  = true
+}
