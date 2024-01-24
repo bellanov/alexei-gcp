1c5c573e912e934bf20f8946e9a52c093ae4a5b4
Bellanov Apilli <bellanovapilli@gmail.com>
2024-01-23 19:45:07 -0500

    Updated application default scripts

--- scripts/application_default_credentials.sh
+++ scripts/application_default_credentials.sh
@@ -3,8 +3,3 @@
 # Generate application default credentials.
 
 gcloud auth application-default login
-
-# Revoke credentials upon completion
-# 
-# Configuration Location: ~/.config/gcloud/application_default_credentials.json
-# gcloud auth application-default revoke
